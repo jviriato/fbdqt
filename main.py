2c66cffc66cd149f20ef240998a5b5f549586f43
José Victor Viriato <jvviriato@inf.ufsm.br>
2017-12-10 15:19:53 -0200

    Funciona uma tabela

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python
 import sys
 
-from PyQt5.QtWidgets import QApplication, QMainWindow
-
+from PyQt5.QtWidgets import *
 from view.mainWindow import Ui_MainWindow
+from PyQt5 import QtSql
 
 from database import Database
 
@@ -12,13 +12,34 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         super(MainWindow, self).__init__()
         self.setupUi(self)
 
+
+    def popularTabela(self, db, table_name):
+        num_rows = db.returnNumRows(table_name)
+        self.tableCurso.setRowCount(num_rows)
+        query = "SELECT * FROM " + table_name
+        db.cur.execute(query)
+        result = db.cur.fetchall()
+        # self.tableCurso.setItem(0,0,QTableWidgetItem(resultado[0]))
+        col = 0
+        for num in range(0, num_rows):
+            for col_number,data in result[num].items():
+                self.tableCurso.setItem(num, col, QTableWidgetItem(str(data)))
+                col = col+1
+                if col == num_rows:
+                    col = 0
+
+
 def main():
     app = QApplication(sys.argv)
     main_window = MainWindow()
     main_window.show()
 
     db = Database("127.0.0.1", "viriato", "dinossauro", "mydb")
-    db.populaTabela("Curso")
+
+    resultado = db.selectAllfromTable("Professor")
+
+    main_window.popularTabela(db, "Curso")
+
     sys.exit(app.exec_())
 
 
