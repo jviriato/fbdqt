5737fa95124985a6f1fe98df5dd0ac78f0c03eff
José Victor Viriato <jvviriato@inf.ufsm.br>
2017-12-10 23:22:42 -0200

    duas telassssss

--- main.py
+++ main.py
@@ -1,107 +1,33 @@
 #!/usr/bin/python
 import sys
 
-from PyQt5.QtWidgets import *
-from view.mainWindow import Ui_MainWindow
-
 from database import Database
+from mainCurso import *
+from view.main import Ui_MainWindow
 
 db = Database("127.0.0.1", "viriato", "dinossauro", "mydb")
-
 
 class MainWindow(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
         self.setupUi(self)
-        self.btnInserir.clicked.connect(lambda: self.adicionar_item_Tabela_Cursos(db, "Curso"));
-        self.btnExcluir.clicked.connect(lambda: self.remover_item_Tabela_Cursos(db, "Curso"));
-        # self.tableCurso.cellClicked.connect(self.cell_was_clicked)
-    def popularTabela(self, db, table_name):
-        num_rows = db.returnNumRows(table_name)
-        self.tableCurso.setRowCount(num_rows)
-        query = "SELECT idCurso, nomeCurso, siglaCurso FROM " + table_name
-        db.cur.execute(query)
-        result = db.cur.fetchall()
-        print(result)
+        self.btnCursos.clicked.connect(lambda: self.clicou_em_Cursos())
 
-        for i, row in enumerate(result):
-            idCurso = row["idCurso"]
-            nomeCurso = row["nomeCurso"]
-            siglaCurso = row["siglaCurso"]
-            self.tableCurso.setItem(i, 0, QTableWidgetItem(str(idCurso)))
-            self.tableCurso.setItem(i, 1, QTableWidgetItem(str(nomeCurso)))
-            self.tableCurso.setItem(i, 2, QTableWidgetItem(str(siglaCurso)))
+    def clicou_em_Cursos(self):
+         window_curso = Curso()
+         window_curso.show()
+         window_curso.popularTabela(db, "Curso")
 
-        # for num, valores in enumerate(result):
-        #     # print(num,valores["siglaCurso"])
-        #     col = 0
-        #     for col_number, data in valores.items():
-        #         print(data)
-        #         self.tableCurso.setItem(num, col, QTableWidgetItem(str(data)))
-        #         col = col + 1
-        #         if col == num_rows:
-        #             col = 0
-
-
-    def check_if_exists_in_db(self, result, siglaCurso, nomeCurso):
-        for i, entry in enumerate(result):
-            if siglaCurso == entry['siglaCurso']:
-                print("Essa sigla já existe!")
-                return 1
-            elif nomeCurso == entry['nomeCurso']:
-                print("Esse curso já existe!")
-                return 1
-        return 0
-
-    def adicionar_item_Tabela_Cursos(self, db, table_name):
-        query = "SELECT * FROM " + table_name
-        db.cur.execute(query)
-        result = db.cur.fetchall()
-        nomeCurso  = self.linenomeCurso.text()
-        siglaCurso = self.lineSiglaCurso.text()
-
-        if(nomeCurso != '' and siglaCurso != ''):
-            num_rows = db.returnNumRows(table_name)
-            self.tableCurso.setRowCount(num_rows + 1)
-
-            if self.check_if_exists_in_db(result, siglaCurso, nomeCurso) == 0:
-                query = "INSERT INTO %s VALUES ('%s', '%s', '%s')" % (
-                table_name, db.returncountId("Curso") + 1, nomeCurso, siglaCurso)
-                print(query)
-                db.cur.execute(query)
-                db.db.commit()
-                self.popularTabela(db, "Curso")
-
-    def remover_item_Tabela_Cursos(self, db, table_name):
-        index = self.tableCurso.currentIndex()
-        row = index.row()
-        column = index.column()
-        # print(row, column)
-
-        #column 2 = id
-        item = self.tableCurso.item(row,0)
-
-        id = int(item.text())
-
-        query = "DELETE FROM %s WHERE idCurso = %d" % (table_name, id)
-        db.cur.execute(query)
-        db.db.commit()
-        self.popularTabela(db, "Curso")
-
-
-
-        # def cell_was_clicked(self, row, column):
-        # print("clique", row, column)
-        # return row
 
 def main():
 
     app = QApplication(sys.argv)
-    main_window = MainWindow()
-    main_window.show()
+    window_main = MainWindow()
+    window_main.show()
 
-
-    main_window.popularTabela(db, "Curso")
+    # window_curso = Curso()
+    # window_curso.show()
+    # window_curso.popularTabela(db, "Curso")
     sys.exit(app.exec_())
 
 
