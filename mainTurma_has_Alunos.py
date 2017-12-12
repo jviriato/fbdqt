3cba3b907ce491b0226957607de071dee3d6e7a8
José Victor Viriato <jvviriato@inf.ufsm.br>
2017-12-12 20:57:02 -0200

    Insere Aluno em Turma

--- mainTurma_has_Alunos.py
+++ mainTurma_has_Alunos.py
@@ -10,10 +10,10 @@ class Turma_has_alunos(QMainWindow, Ui_Alunos_em_Turmas):
         header = self.tableTurma_has_Alunos.setColumnWidth(1, 300)
         header = self.tableTurma_has_Alunos.setColumnWidth(2, 90)
         header = self.tableTurma_has_Alunos.setColumnWidth(3, 300)
-        self.btnInserir.clicked.connect(lambda: self.adicionar_item_Tabela_Turma_has_Alunos(db, "Turma_has_Alunos"))
-        self.btnExcluir.clicked.connect(lambda: self.remover_item_Tabela_Turma_has_Alunos(db, "Turma_has_Alunos"))
-        self.btnEditar.clicked.connect(lambda: self.editar_item_Tabela_Turma_has_Alunos(db, "Turma_has_Alunos"))
-        self.lineBusca.textChanged.connect(lambda: self.buscar_item_Tabela_Turma_has_Alunos(db, "Turma_has_Alunos"))
+        self.btnInserir.clicked.connect(lambda: self.adicionar_item_Tabela_Turma_has_Alunos(db, "Frequencia"))
+        self.btnExcluir.clicked.connect(lambda: self.remover_item_Tabela_Turma_has_Alunos(db, "Frequencia"))
+        self.btnEditar.clicked.connect(lambda: self.editar_item_Tabela_Turma_has_Alunos(db, "Frequencia"))
+        self.lineBusca.textChanged.connect(lambda: self.buscar_item_Tabela_Turma_has_Alunos(db, "vw_Turm_has_Alunos"))
 
     def popularTabela(self, db, table_name):
         num_rows = db.returnNumRows(table_name)
@@ -30,14 +30,14 @@ class Turma_has_alunos(QMainWindow, Ui_Alunos_em_Turmas):
             nomeTurma = row["nomeTurma"]
             self.tableTurma_has_Alunos.setItem(i, 0, QTableWidgetItem(str(idAluno)))
             self.tableTurma_has_Alunos.setItem(i, 1, QTableWidgetItem(str(nomeAluno)))
-            self.tableTurma_has_Alunos.setItem(i, 2, QTableWidgetItem(str(idAluno)))
+            self.tableTurma_has_Alunos.setItem(i, 2, QTableWidgetItem(str(idTurma)))
             self.tableTurma_has_Alunos.setItem(i, 3, QTableWidgetItem(str(nomeTurma)))
 
-    def check_if_exists_in_db(self, result, nomeDisciplina, nomeCurso):
+    def check_if_exists_in_db(self, result, nomeAluno, nomeTurma):
         for i, entry in enumerate(result):
-            if nomeDisciplina == entry['nomeDisciplina'] and nomeCurso == entry['nomeCurso']:
+            if nomeAluno == entry['nomeAluno'] and nomeTurma == entry['nomeTurma']:
                 erro = QMessageBox()
-                erro.setText("Essa Disciplina já está no Curso!")
+                erro.setText("Esse Aluno já está na Turma!")
                 erro.exec()
                 return 1
         return 0
@@ -46,50 +46,55 @@ class Turma_has_alunos(QMainWindow, Ui_Alunos_em_Turmas):
         query = "SELECT * FROM " + "vw_Turma_has_Alunos"
         db.cur.execute(query)
         result = db.cur.fetchall()
-        nomeDisciplina  = self.linenomeDisciplina.text()
-        nomeCurso  = self.lineNomeCurso.text()
-        if(nomeDisciplina != '' and nomeCurso != ''):
+        nomeAluno  = self.linenomeAluno.text()
+        nomeTurma  = self.lineNomeTurma.text()
+        matricula  = self.lineMatricula.text()  
+        if(nomeAluno != '' and nomeTurma != ''):
             num_rows = db.returnNumRows(table_name)
 
-            if self.check_if_exists_in_db(result, nomeDisciplina, nomeCurso) == 0:
-                query = "SELECT idCurso FROM Curso WHERE nomeCurso = '{0}'".format(nomeCurso)
+            if self.check_if_exists_in_db(result, nomeAluno, nomeTurma) == 0:
+                query = "SELECT idTurma FROM Turma WHERE nomeTurma = '{0}'".format(nomeTurma)
                 db.cur.execute(query)
                 result = db.cur.fetchall()
                 if result == ():
                     erro = QMessageBox()
-                    erro.setText("Esse Curso não existe")
+                    erro.setText("Essa Turma não existe")
                     erro.exec()
                     return
                 else:
                     for i, data in enumerate(result):
-                        idCurso = data["idCurso"]
-                query = "SELECT idDisciplina FROM Disciplina WHERE nomeDisciplina = '{0}'".format(nomeDisciplina)
+                        idTurma = data["idTurma"]
+                query = "SELECT idAluno FROM Aluno WHERE nomeAluno = '{0}'".format(nomeAluno)
                 db.cur.execute(query)
                 result = db.cur.fetchall()
-                #se a disciplina nao existe
+                #se a turma nao existe
                 if result == ():
-                    #create DISCIPLINA
-                    query = "INSERT INTO Disciplina VALUES(NULL, '{0}')".format(nomeDisciplina)
+                    #create ALUNO
+                    query = "INSERT INTO Aluno VALUES(NULL, '{0}', '{1}')".format(nomeAluno, matricula)
+
                     db.cur.execute(query)
+                    print("query", query)
+                    print("cur:",db.cur.fetchall())
+
                     db.db.commit()
-                    query = "SELECT idDisciplina FROM Disciplina WHERE nomeDisciplina = '{0}'".format(nomeDisciplina)
+                    query = "SELECT idAluno FROM Aluno WHERE nomeAluno = '{0}'".format(nomeAluno)
                     db.cur.execute(query)
                     result = db.cur.fetchall()
                     for i, data in enumerate(result):
-                        idDisciplina = data["idDisciplina"]
+                        idAluno = data["idAluno"]
                     #insert into C_h_D
-                    query = "INSERT INTO Turma_has_Alunos VALUES('{0}', '{1}')".format(idCurso, idDisciplina)
+                    query = "INSERT INTO Frequencia VALUES(NULL, '{0}', '{1}', NULL, NULL)".format(idTurma, idAluno)
                     db.cur.execute(query)
                     db.db.commit()
                 else:
-                    #add idDisciplina, idCurso no Turma_has_Alunos
+                    #add idAluno, idTurma no Turma_has_Alunos
                     for i, data in enumerate(result):
-                        idDisciplina = data["idDisciplina"]
-                    query = "INSERT INTO Turma_has_Alunos VALUES('{0}', '{1}')".format(idCurso, idDisciplina)
+                        idAluno = data["idAluno"]
+                    query = "INSERT INTO Frequencia VALUES(NULL, '{0}', '{1}', NULL, NULL)".format(idTurma, idAluno)
                     db.cur.execute(query)
                     db.db.commit()
 
-                # query = "INSERT INTO %s VALUES (NULL, '%s', '%s')" % (Turma_has_Alunos, idCurso, idDisciplina)
+                # query = "INSERT INTO %s VALUES (NULL, '%s', '%s')" % (Turma_has_Alunos, idTurma, idAluno)
                 # print(query)
                 # db.cur.execute(query)
                 # db.db.commit()
@@ -103,19 +108,19 @@ class Turma_has_alunos(QMainWindow, Ui_Alunos_em_Turmas):
 
         #column 0 = id
         item = self.tableTurma_has_Alunos.item(row,2)
-        idDisciplina = int(item.text())
+        idAluno = int(item.text())
 
         item = self.tableTurma_has_Alunos.item(row, 0)
-        idCurso = int(item.text())
-#       remover disciplina do Curso
-        query = "DELETE FROM Turma_has_Alunos WHERE Disciplina_idDisciplina = {0} AND Curso_idCurso = {1}".format(idDisciplina, idCurso)
+        idTurma = int(item.text())
+#       remover aluno do Turma
+        query = "DELETE FROM Frequencia WHERE Aluno_idAluno = {0} AND Turma_idTurma = {1}".format(idAluno, idTurma)
         print(query)
         db.cur.execute(query)
         db.db.commit()
         self.popularTabela(db, "vw_Turma_has_Alunos")
 
         # if self.check_if_exists_in_another_table(id) == 0:
-        #     query = "DELETE FROM %s WHERE Curso_idCurso = %d" % (table_name, id)
+        #     query = "DELETE FROM %s WHERE Turma_idTurma = %d" % (table_name, id)
         #     db.cur.execute(query)
         #     db.db.commit()
         #     self.popularTabela(db, "vw_Turma_has_Alunos")
@@ -130,36 +135,36 @@ class Turma_has_alunos(QMainWindow, Ui_Alunos_em_Turmas):
         row = index.row()
         column = index.column()
 
-        idCurso = int(self.tableTurma_has_Alunos.item(row,0).text())
-        nomeCurso = str(self.tableTurma_has_Alunos.item(row,1).text())
-        idDisciplina = int(self.tableTurma_has_Alunos.item(row,2).text())
-        nomeDisciplina = str(self.tableTurma_has_Alunos.item(row,3).text())
+        idTurma = int(self.tableTurma_has_Alunos.item(row,0).text())
+        nomeTurma = str(self.tableTurma_has_Alunos.item(row,1).text())
+        idAluno = int(self.tableTurma_has_Alunos.item(row,2).text())
+        nomeAluno = str(self.tableTurma_has_Alunos.item(row,3).text())
 
-        query = "SELECT nomeCurso, idCurso FROM Curso WHERE nomeCurso = '{0}'".format(nomeCurso)
+        query = "SELECT nomeTurma, idTurma FROM Turma WHERE nomeTurma = '{0}'".format(nomeTurma)
         result = db.cur.execute(query);
         if result == 0:
-            #curso não existe, só dar update
-            query = "UPDATE Curso SET nomeCurso = '{0}' WHERE idCurso = '{1}'".format(nomeCurso, idCurso)
+            #turma não existe, só dar update
+            query = "UPDATE Turma SET nomeTurma = '{0}' WHERE idTurma = '{1}'".format(nomeTurma, idTurma)
             db.cur.execute(query)
             db.db.commit()
         else:
-            #curso existe, mudar ids
+            #turma existe, mudar ids
             valor = db.cur.fetchall()
             for i, row in enumerate(valor):
-                idCursoNEW = row["idCurso"]
+                idTurmaNEW = row["idTurma"]
 
-            query = "UPDATE Curso SET idCurso = '{0}' WHERE idCurso = '{1}'".format(idCursoNEW, idCurso)
+            query = "UPDATE Turma SET idTurma = '{0}' WHERE idTurma = '{1}'".format(idTurmaNEW, idTurma)
             db.cur.execute(query)
             db.db.commit()
 
 
-        # query = "SELECT nomeDisciplina FROM Disciplina WHERE nomeDisciplina = '{0}'".format(nomeDisciplina)
+        # query = "SELECT nomeAluno FROM Aluno WHERE nomeAluno = '{0}'".format(nomeAluno)
         # result = db.cur.execute(query)
         # if result == 0:
-        #     #disciplina não existe
-        #     #cria disciplina
+        #     #aluno não existe
+        #     #cria aluno
         # else:
-        #     #disciplina existe
+        #     #aluno existe
 
 
         self.popularTabela(db, "vw_Turma_has_Alunos")
@@ -178,5 +183,5 @@ class Turma_has_alunos(QMainWindow, Ui_Alunos_em_Turmas):
             nomeTurma = row["nomeTurma"]
             self.tableTurma_has_Alunos.setItem(i, 0, QTableWidgetItem(str(idAluno)))
             self.tableTurma_has_Alunos.setItem(i, 1, QTableWidgetItem(str(nomeAluno)))
-            self.tableTurma_has_Alunos.setItem(i, 2, QTableWidgetItem(str(idAluno)))
+            self.tableTurma_has_Alunos.setItem(i, 2, QTableWidgetItem(str(idTurma)))
             self.tableTurma_has_Alunos.setItem(i, 3, QTableWidgetItem(str(nomeTurma)))
